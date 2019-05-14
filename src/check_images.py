c41c7701eb54907abffdbb7233f02cd02b5e8549
tylerecouture <tylerecouture@gmail.com>
2019-05-14 13:49:29 -0700

    update TODO numbering

--- src/check_images.py
+++ src/check_images.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
-# TODO -1: Add your information below for Programmer & Date Created.                                                                             
+# TODO 0: Add your information below for Programmer & Date Created.                                                                             
 # PROGRAMMER: 
 # DATE CREATED:                                  
 # REVISED DATE: 
@@ -39,10 +39,10 @@ from print_results import print_results
 
 # Main program function defined below
 def main():
-    # TODO 0: Measures total program runtime by collecting start time
+    # TODO 1: Measures total program runtime by collecting start time
     start_time = time()
     
-    # TODO 1: Define get_input_args function within the file get_input_args.py
+    # TODO 2: Define get_input_args function within the file get_input_args.py
     # This function retrieves 3 Command Line Arugments from user as input from
     # the user running the program from a terminal window. This function returns
     # the collection of these command line arguments from the function call as
@@ -53,7 +53,7 @@ def main():
     check_command_line_arguments(in_arg)
 
     
-    # TODO 2: Define get_pet_labels function within the file get_pet_labels.py
+    # TODO 3: Define get_pet_labels function within the file get_pet_labels.py
     # Once the get_pet_labels function has been defined replace 'None' 
     # in the function call with in_arg.dir  Once you have done the replacements
     # your function call should look like this: 
@@ -66,7 +66,7 @@ def main():
     check_creating_pet_image_labels(results)
 
 
-    # TODO 3: Define classify_images function within the file classify_images.py
+    # TODO 4: Define classify_images function within the file classify_images.py
     # Once the classify_images function has been defined replace first 'None' 
     # in the function call with in_arg.dir and replace the last 'None' in the
     # function call with in_arg.arch  Once you have done the replacements your
@@ -80,7 +80,7 @@ def main():
     check_classifying_images(results)    
 
     
-    # TODO 4: Define adjust_results4_isadog function within the file adjust_results4_isadog.py
+    # TODO 5: Define adjust_results4_isadog function within the file adjust_results4_isadog.py
     # Once the adjust_results4_isadog function has been defined replace 'None' 
     # in the function call with in_arg.dogfile  Once you have done the 
     # replacements your function call should look like this: 
@@ -94,7 +94,7 @@ def main():
     check_classifying_labels_as_dogs(results)
 
 
-    # TODO 5: Define calculates_results_stats function within the file calculates_results_stats.py
+    # TODO 6: Define calculates_results_stats function within the file calculates_results_stats.py
     # This function creates the results statistics dictionary that contains a
     # summary of the results statistics (this includes counts & percentages). This
     # dictionary is returned from the function call as the variable results_stats    
@@ -106,7 +106,7 @@ def main():
     check_calculating_results(results, results_stats)
 
 
-    # TODO 6: Define print_results function within the file print_results.py
+    # TODO 7: Define print_results function within the file print_results.py
     # Once the print_results function has been defined replace 'None' 
     # in the function call with in_arg.arch  Once you have done the 
     # replacements your function call should look like this: 
@@ -115,10 +115,10 @@ def main():
     # and incorrectly classified breeds (if requested)
     print_results(results, results_stats, None, True, True)
     
-    # TODO 0: Measure total program runtime by collecting end time
+    # TODO 1: Measure total program runtime by collecting end time
     end_time = time()
     
-    # TODO 0: Computes overall runtime in seconds & prints it in hh:mm:ss format
+    # TODO 1: Computes overall runtime in seconds & prints it in hh:mm:ss format
     tot_time = end_time - start_time
     print("\n** Total Elapsed Runtime:",
           str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
