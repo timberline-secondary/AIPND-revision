c6043520eeb9bfc854537e349b7814cc92f69283
tylerecouture <tylerecouture@gmail.com>
2019-05-15 15:58:14 -0700

    no need to reference torch < 0.4

--- src/classifier.py
+++ src/classifier.py
@@ -33,24 +33,13 @@ def classifier(img_path, model_name):
     # resize the tensor (add dimension for batch)
     img_tensor.unsqueeze_(0)
     
-    # wrap input in variable, wrap input in variable - no longer needed for
-    # v 0.4 & higher code changed 04/26/2018 by Jennifer S. to handle PyTorch upgrade
-    pytorch_ver = __version__.split('.')
-    
     # pytorch versions 0.4 & hihger - Variable depreciated so that it returns
     # a tensor. So to address tensor as output (not wrapper) and to mimic the 
     # affect of setting volatile = True (because we are using pretrained models
     # for inference) we can set requires_gradient to False. Here we just set 
     # requires_grad_ to False on our tensor 
-    if int(pytorch_ver[0]) > 0 or int(pytorch_ver[1]) >= 4:
-        img_tensor.requires_grad_(False)
+    img_tensor.requires_grad_(False)
     
-    # pytorch versions less than 0.4 - uses Variable because not-depreciated
-    else:
-        # apply model to input
-        # wrap input in variable
-        data = Variable(img_tensor, volatile = True) 
-
     # apply model to input
     model = models[model_name]
 
