df4209d9f853a93d39d2c8a1a54c5414cb175e20
tylerecouture <tylerecouture@gmail.com>
2019-05-16 16:47:14 -0700

    Print results completed

--- src/print_results.py
+++ src/print_results.py
@@ -80,7 +80,7 @@ def print_results(results_dic, results_stats_dic, model,
     #          that's accessed by key 'n_notdogs_img' using dictionary 
     #          results_stats_dic
     #
-    print("")
+    print("{:20}: {:3d}".format('N Not-Dog Imagesages', results_stats_dic['n_notdogs_img']))
 
 
     # Prints summary statistics (percentages) on Model Run
@@ -95,7 +95,8 @@ def print_results(results_dic, results_stats_dic, model,
         #           both the key and the value. Remember the value is accessed 
         #           by results_stats_dic[key]
         #
-        pass 
+        if "pct" in key:
+            print("{:20}: {:3f}".format(key, results_stats_dic[key])) 
 
 
     # IF print_incorrect_dogs == True AND there were images incorrectly 
@@ -127,7 +128,12 @@ def print_results(results_dic, results_stats_dic, model,
             #
             # Pet Image Label is a Dog - Classified as NOT-A-DOG -OR- 
             # Pet Image Label is NOT-a-Dog - Classified as a-DOG
-            pass
+            print("\nINCORRECT Species Assignment:")
+
+            if results_dic[key][3] + results_dic[key][4] == 1:
+                print("Real: {:>26}   Classifier: {:>30}".format(results_dic[key][0],
+                                                          results_dic[key][1]))    
+
 
     # IF print_incorrect_breed == True AND there were dogs whose breeds 
     # were incorrectly classified - print out these cases                    
